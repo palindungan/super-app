0260398864e4ee90a270e2b47ff495be426eb003
palindungan <rizkika.palindungan1@gmail.com>
2026-02-27 15:30:34 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,5 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth'])->group(function () {
     Route::get('/', function () {
         return view('components/layouts/master_1/main');
-    });
+    })->name('home');
 });
