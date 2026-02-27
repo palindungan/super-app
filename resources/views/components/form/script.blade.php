240deb337ae38a01c7bd066da2386e22a20a2a63
palindungan <rizkika.palindungan1@gmail.com>
2026-02-27 16:00:20 +0700

    Update script.blade.php

--- resources/views/components/form/script.blade.php
+++ resources/views/components/form/script.blade.php
@@ -3,7 +3,10 @@
         const button = form.querySelector('button[type="submit"]');
         if (button) {
             button.disabled = true;
-            button.innerHTML = 'Memuat...';
+            button.innerHTML = `
+                <span class="spinner-border spinner-border-sm" aria-hidden="true"></span>
+                <span role="status">Memuat...</span>
+            `;
         }
     }
 </script>
