d3658f2e0622fa48740c1dbd22394b6ba7c039d8
palindungan <rizkika.palindungan1@gmail.com>
2026-03-17 15:37:19 +0700

    Update create.blade.php

--- Modules/AdministratorModule/resources/views/roles/create.blade.php
+++ Modules/AdministratorModule/resources/views/roles/create.blade.php
@@ -12,4 +12,46 @@
             ],
         ])
     </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="largeInput">Nama</label>
+                                <input type="text" class="form-control form-control" id="defaultInput"
+                                    placeholder="Nama" />
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <label for="largeInput">Nama Penjaga</label>
+                                <input type="text" class="form-control form-control" id="defaultInput"
+                                    placeholder="Nama Penjaga" />
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-group">
+                                <div class="form-check form-switch">
+                                    <input class="form-check-input" type="checkbox" role="switch"
+                                        id="flexSwitchCheckDefault">
+                                    <label class="form-check-label" for="flexSwitchCheckDefault">Pilih Semua</label>
+                                    <br>
+                                    <small id="emailHelp2" class="form-text text-muted">Aktifkan semua izin yang Tersedia
+                                        untuk Peran ini.</small>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-12">
+            <button class="btn btn-success">Buat</button>
+            <button class="btn btn-danger">Batal</button>
+        </div>
+    </div>
 @endsection
