3004e55b94332dd7e7a5edc087cc5e23a2846e8d
palindungan <rizkika.palindungan1@gmail.com>
2026-03-18 14:40:57 +0700

    r

--- Modules/AdministratorModule/resources/views/roles/fields.blade.php
+++ Modules/AdministratorModule/resources/views/roles/fields.blade.php
@@ -17,13 +17,16 @@
                 <div class="col-md-4">
                     <div class="form-group">
                         <div class="form-check form-switch">
-                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
-                            <label class="form-check-label" for="flexSwitchCheckDefault">Pilih Semua</label>
+                            <input class="form-check-input" type="checkbox" role="switch" id="select_all"
+                                name="select_all">
+                            <label class="form-check-label" for="select_all">
+                                Pilih Semua
+                            </label>
                             <br>
-                            <small id="emailHelp2" class="form-text text-muted">Aktifkan semua izin yang Tersedia
-                                untuk Peran ini.</small>
+                            <small id="emailHelp2" class="form-text text-muted">
+                                Aktifkan semua izin yang Tersedia untuk Peran ini.
+                            </small>
                         </div>
-
                     </div>
                 </div>
             </div>
@@ -37,27 +40,39 @@
                 <ul class="nav nav-tabs nav-line nav-color-secondary" id="line-tab" role="tablist">
                     <li class="nav-item">
                         <a class="nav-link active" id="line-1-tab" data-bs-toggle="pill" href="#line-1" role="tab"
-                            aria-controls="pills-1" aria-selected="true">Utama</a>
+                            aria-controls="pills-1" aria-selected="true">
+                            Utama
+                        </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" id="line-2-tab" data-bs-toggle="pill" href="#line-2" role="tab"
-                            aria-controls="pills-2" aria-selected="false">Laporan</a>
+                            aria-controls="pills-2" aria-selected="false">
+                            Laporan
+                        </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" id="line-3-tab" data-bs-toggle="pill" href="#line-3" role="tab"
-                            aria-controls="pills-3" aria-selected="false">Data Produk</a>
+                            aria-controls="pills-3" aria-selected="false">
+                            Data Produk
+                        </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" id="line-4-tab" data-bs-toggle="pill" href="#line-4" role="tab"
-                            aria-controls="pills-4" aria-selected="false">Data Master</a>
+                            aria-controls="pills-4" aria-selected="false">
+                            Data Master
+                        </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" id="line-5-tab" data-bs-toggle="pill" href="#line-5" role="tab"
-                            aria-controls="pills-5" aria-selected="false">Lain-Lain</a>
+                            aria-controls="pills-5" aria-selected="false">
+                            Lain-Lain
+                        </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" id="line-6-tab" data-bs-toggle="pill" href="#line-6" role="tab"
-                            aria-controls="pills-6" aria-selected="false">Administrator</a>
+                            aria-controls="pills-6" aria-selected="false">
+                            Administrator
+                        </a>
                     </li>
                 </ul>
                 <div class="tab-content mt-3 mb-3" id="line-tabContent">
