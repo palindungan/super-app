e1aaedd0ca13393643c465bf8fbbea488ad39b81
palindungan <rizkika.palindungan1@gmail.com>
2026-03-18 15:48:38 +0700

    Update Role.php

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -6,5 +6,46 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    //
+    public static array $data = [
+        [
+            'label' => 'Utama',
+            'data' => [
+                [
+                    'label' => 'Beranda',
+                    'permissions' => [
+                        [
+                            'label' => 'Lihat Apa Saja',
+                            'name' => 'home.index',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+        [
+            'label' => 'Administrator',
+            'data' => [
+                [
+                    'label' => 'Peran dan Izin',
+                    'permissions' => [
+                        [
+                            'label' => 'Lihat Apa Saja',
+                            'name' => 'administrator-roles.index',
+                        ],
+                        [
+                            'label' => 'Lihat Apa Saja',
+                            'name' => 'administrator-roles.create',
+                        ],
+                        [
+                            'label' => 'Lihat Apa Saja',
+                            'name' => 'administrator-roles.edit',
+                        ],
+                        [
+                            'label' => 'Lihat Apa Saja',
+                            'name' => 'administrator-roles.destroy',
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ];
 }
