84335298c2a192ee10201cf863519572c56fb25a
palindungan <rizkika.palindungan1@gmail.com>
2026-03-18 14:37:08 +0700

    Update fields.blade.php

---
+++ Modules/AdministratorModule/resources/views/roles/fields.blade.php
@@ -0,0 +1,175 @@
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-body">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label for="largeInput">Nama</label>
+                        <input type="text" class="form-control form-control" id="name" name="name" />
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <label for="largeInput">Nama Penjaga</label>
+                        <input type="text" class="form-control form-control" id="guard_name" name="guard_name" />
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="form-group">
+                        <div class="form-check form-switch">
+                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault">
+                            <label class="form-check-label" for="flexSwitchCheckDefault">Pilih Semua</label>
+                            <br>
+                            <small id="emailHelp2" class="form-text text-muted">Aktifkan semua izin yang Tersedia
+                                untuk Peran ini.</small>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="col-md-12">
+    <div class="card">
+        <div class="card-body pb-0" style="padding-top: 0px;">
+            <div class="col-md-12 pb-0" style="padding: 10px;">
+                <ul class="nav nav-tabs nav-line nav-color-secondary" id="line-tab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="line-1-tab" data-bs-toggle="pill" href="#line-1" role="tab"
+                            aria-controls="pills-1" aria-selected="true">Utama</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="line-2-tab" data-bs-toggle="pill" href="#line-2" role="tab"
+                            aria-controls="pills-2" aria-selected="false">Laporan</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="line-3-tab" data-bs-toggle="pill" href="#line-3" role="tab"
+                            aria-controls="pills-3" aria-selected="false">Data Produk</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="line-4-tab" data-bs-toggle="pill" href="#line-4" role="tab"
+                            aria-controls="pills-4" aria-selected="false">Data Master</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="line-5-tab" data-bs-toggle="pill" href="#line-5" role="tab"
+                            aria-controls="pills-5" aria-selected="false">Lain-Lain</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="line-6-tab" data-bs-toggle="pill" href="#line-6" role="tab"
+                            aria-controls="pills-6" aria-selected="false">Administrator</a>
+                    </li>
+                </ul>
+                <div class="tab-content mt-3 mb-3" id="line-tabContent">
+                    <div class="tab-pane fade show active" id="line-1" role="tabpanel" aria-labelledby="line-1-tab">
+                        <p>Utama</p>
+                        <div class="accordion accordion-secondary">
+                            <div class="card">
+                                <div class="card-header" id="heading1" data-bs-toggle="collapse"
+                                    data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
+                                    <div class="span-title">
+                                        Lorem Ipsum #1
+                                    </div>
+                                    <div class="span-mode"></div>
+                                </div>
+                                <div id="collapse1" class="collapse show" aria-labelledby="heading1"
+                                    data-parent="#accordion">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-md-3">
+                                                <div class="form-check">
+                                                    <input type="checkbox" class="form-check-input"
+                                                        id="exampleCheck1">
+                                                    <label class="form-check-label" for="exampleCheck1">
+                                                        Check me out
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <div class="form-check">
+                                                    <input type="checkbox" class="form-check-input"
+                                                        id="exampleCheck1">
+                                                    <label class="form-check-label" for="exampleCheck1">
+                                                        Check me out
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <div class="form-check">
+                                                    <input type="checkbox" class="form-check-input"
+                                                        id="exampleCheck1">
+                                                    <label class="form-check-label" for="exampleCheck1">
+                                                        Check me out
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <div class="form-check">
+                                                    <input type="checkbox" class="form-check-input"
+                                                        id="exampleCheck1">
+                                                    <label class="form-check-label" for="exampleCheck1">
+                                                        Check me out
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <div class="form-check">
+                                                    <input type="checkbox" class="form-check-input"
+                                                        id="exampleCheck1">
+                                                    <label class="form-check-label" for="exampleCheck1">
+                                                        Check me out
+                                                    </label>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <div class="form-check">
+                                                    <input type="checkbox" class="form-check-input"
+                                                        id="exampleCheck1">
+                                                    <label class="form-check-label" for="exampleCheck1">
+                                                        Check me out
+                                                    </label>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="line-2" role="tabpanel" aria-labelledby="line-2-tab">
+                        <p>Laporan</p>
+                        <div class="accordion accordion-secondary">
+                            <div class="card">
+                                <div class="card-header" id="heading1" data-bs-toggle="collapse"
+                                    data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">
+                                    <div class="span-title">
+                                        Lorem Ipsum #1
+                                    </div>
+                                    <div class="span-mode"></div>
+                                </div>
+                                <div id="collapse1" class="collapse show" aria-labelledby="heading1"
+                                    data-parent="#accordion">
+                                    <div class="card-body">
+                                        ----------
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="tab-pane fade" id="line-3" role="tabpanel" aria-labelledby="line-3-tab">
+                        <p>Data Produk</p>
+                    </div>
+                    <div class="tab-pane fade" id="line-4" role="tabpanel" aria-labelledby="line-4-tab">
+                        <p>Data Master</p>
+                    </div>
+                    <div class="tab-pane fade" id="line-5" role="tabpanel" aria-labelledby="line-5-tab">
+                        <p>Lain-Lain</p>
+                    </div>
+                    <div class="tab-pane fade" id="line-6" role="tabpanel" aria-labelledby="line-6-tab">
+                        <p>Administrator</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
