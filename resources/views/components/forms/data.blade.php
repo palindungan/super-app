ec790d2fdd73301299e08a9033f43fb9195f6d77
palindungan <rizkika.palindungan1@gmail.com>
2026-03-28 10:36:07 +0700

    Update data.blade.php

--- resources/views/components/forms/data.blade.php
+++ resources/views/components/forms/data.blade.php
@@ -1,3 +1,13 @@
+@php
+    use Illuminate\Support\Facades\Crypt;
+
+    // Dapatkan timestamp saat ini (detik)
+    $nowTimestamp = now()->timestamp; // integer
+
+    // Convert ke string sebelum encrypt
+    $token = Crypt::encryptString((string) $nowTimestamp);
+@endphp
+
 @method($method)
 @csrf
-<input type="hidden" name="_token_form" value="{{ Str::uuid() }}" autocomplete="off">
+<input type="hidden" name="_token_form" value="{{ $token }}" autocomplete="off">
