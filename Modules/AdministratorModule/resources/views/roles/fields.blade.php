0d1957cf250d4aa3813cd92767deb96cd36d6532
palindungan <rizkika.palindungan1@gmail.com>
2026-03-28 10:16:10 +0700

    Update fields.blade.php

--- Modules/AdministratorModule/resources/views/roles/fields.blade.php
+++ Modules/AdministratorModule/resources/views/roles/fields.blade.php
@@ -1,3 +1,9 @@
+@push('scripts')
+    <script src="{{ asset('components/layouts/master_1/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}">
+    </script>
+    @include('components.notifications.bootstrap_notify')
+@endpush
+
 <div class="col-md-12">
     <div class="card">
         <div class="card-body">
