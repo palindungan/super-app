06334b47b3d2f072fae3046055570522eea1d595
palindungan <rizkika.palindungan1@gmail.com>
2026-03-28 12:56:21 +0700

    Update helpers.php

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Crypt;
+use Illuminate\Support\Str;
 
 if (!function_exists('token_form_generate')) {
     function token_form_generate()
@@ -9,9 +10,20 @@ if (!function_exists('token_form_generate')) {
         $nowTimestamp = now()->timestamp; // integer
 
         // Convert ke string sebelum encrypt
-        $token = Crypt::encryptString((string) $nowTimestamp);
+        $token = Crypt::encryptString((string) $nowTimestamp . "-token_form_generate");
 
         return $token;
+    }
+}
+
+if (!function_exists('token_form_decrypt')) {
+    function token_form_decrypt(string $token)
+    {
+        try {
+            return Crypt::decryptString($token);
+        } catch (\Exception $e) {
+            return null; // invalid token
+        }
     }
 }
 
@@ -20,8 +32,14 @@ if (!function_exists('form_token_check')) {
     {
         $_token_form = $request->input('_token_form');
 
+        if (Str::contains(token_form_decrypt($_token_form), 'token_form_generate')) {
+            # code...
+        } else {
+            return redirect($url)->withInput()->with('error', 'Ups, token invalid');
+        }
+
         if (session()->has('last_token_form') && session('last_token_form') === $_token_form) {
-            return redirect($url)->withInput()->with('error', 'Ups, kamu menekan tombol simpan dua kali.');
+            return redirect($url)->withInput()->with('error', 'Ups, kamu menekan tombol simpan dua kali');
         }
 
         session(['last_token_form' => $_token_form]);
