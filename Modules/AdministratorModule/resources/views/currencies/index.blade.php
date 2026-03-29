f61cdf7484d2f2281cf56dbbdb938de4ef47d62e
palindungan <rizkika.palindungan1@gmail.com>
2026-03-29 10:30:55 +0700

    Update index.blade.php

---
+++ Modules/AdministratorModule/resources/views/currencies/index.blade.php
@@ -0,0 +1,36 @@
+@extends('components.layouts.master_1.main')
+
+@section('sidebar.administrator.currencies.active', 'active')
+
+@include('components.resources.assets.index')
+
+@section('content')
+    <div class="page-header">
+        <h3 class="fw-bold mb-3">Mata Uang</h3>
+        @include('components.layouts.master_1.breadcrumb.main', [
+            'breadcrumbs' => [
+                ['label' => 'Mata Uang', 'url' => route('administrator-currencies.index')],
+                ['label' => 'Daftar'],
+            ],
+        ])
+    </div>
+
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center">
+                        <h4 class="card-title">Mata Uang</h4>
+                        <a class="btn btn-primary btn-round ms-auto" href="{{ route('administrator-currencies.create') }}">
+                            <i class="fa fa-plus"></i>
+                            Buat Mata Uang
+                        </a>
+                    </div>
+                </div>
+                <div class="card-body">
+                    @include('administratormodule::currencies.table')
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
