a8202abf0760c9078fdff8b13d6d31b5ce27abc0
palindungan <rizkika.palindungan1@gmail.com>
2026-03-29 10:43:49 +0700

    Update table.blade.php

--- Modules/AdministratorModule/resources/views/currencies/table.blade.php
+++ Modules/AdministratorModule/resources/views/currencies/table.blade.php
@@ -13,3 +13,70 @@
         <tbody></tbody>
     </table>
 </div>
+
+@push('scripts')
+    <script>
+        let datatable;
+        $(document).ready(function() {
+            datatable = new DataTable('#datatable', {
+                language: {
+                    url: "{{ asset('assets/json/plugin/datatables/id.json') }}"
+                },
+                processing: true,
+                serverSide: true,
+                ajax: {
+                    url: "{{ url()->current() }}",
+                    data: function(d) {
+                        d.datatable = 'main';
+                    },
+                    error: function(xhr) {
+                        try {
+                            console.error(JSON.parse(xhr.responseText));
+                        } catch (e) {
+                            console.error(xhr.responseText);
+                        }
+                    }
+                },
+                order: [
+                    [0, 'asc']
+                ],
+                columns: [{
+                        data: 'code',
+                        name: 'currencies.code',
+                        defaultContent: '-'
+                    },
+                    {
+                        data: 'name',
+                        name: 'currencies.name',
+                        defaultContent: '-'
+                    },
+                    {
+                        data: 'symbol',
+                        name: 'currencies.symbol',
+                        defaultContent: '-'
+                    },
+                    {
+                        data: 'minor_unit',
+                        name: 'currencies.minor_unit',
+                        defaultContent: '-'
+                    },
+                    {
+                        data: 'is_active',
+                        name: 'currencies.is_active',
+                        defaultContent: '-'
+                    },
+                    {
+                        data: 'action',
+                        name: 'currencies.id',
+                        className: 'text-end',
+                        defaultContent: '-',
+                        orderable: false,
+                        searchable: false
+                    }
+                ],
+                stateSave: true,
+                searchDelay: 500
+            });
+        });
+    </script>
+@endpush
