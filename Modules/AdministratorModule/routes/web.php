db539f785df583b81ea01dffcc6f89e33de2f501
palindungan <rizkika.palindungan1@gmail.com>
2026-03-29 10:04:09 +0700

    Update web.php

--- Modules/AdministratorModule/routes/web.php
+++ Modules/AdministratorModule/routes/web.php
@@ -4,5 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Modules\AdministratorModule\Http\Controllers\RoleController;
 
 Route::prefix('administrator')->middleware(['auth'])->group(function () {
+    Route::resource('currencies', RoleController::class)->names('administrator-currencies');
     Route::resource('roles', RoleController::class)->names('administrator-roles');
 });
