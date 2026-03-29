c9596879916656ddb0b868554ced470db525cea3
palindungan <rizkika.palindungan1@gmail.com>
2026-03-29 10:35:03 +0700

    Update table.blade.php

---
+++ Modules/AdministratorModule/resources/views/currencies/table.blade.php
@@ -0,0 +1,15 @@
+<div class="table-responsive">
+    <table id="datatable" class="display table table-striped table-hover">
+        <thead>
+            <tr>
+                <th>Kode</th>
+                <th>Nama</th>
+                <th>Simbol</th>
+                <th>Satuan Pecahan</th>
+                <th>Aktif</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    </table>
+</div>
