ee16e021b6366b3d9f3a979ecdba98a145749fb4
palindungan <rizkika.palindungan1@gmail.com>
2026-03-31 11:13:33 +0700

    Update edit.blade.php

--- Modules/AdministratorModule/resources/views/currencies/edit.blade.php
+++ Modules/AdministratorModule/resources/views/currencies/edit.blade.php
@@ -1,12 +1,31 @@
 @push('scripts')
     <script>
         async function editAction(url) {
+            const $form = $('#fields_form');
+            formReset($form);
+
+            validationErrorsReset();
+
             try {
                 const response = await showApi(url);
-                console.log(response);
+                editInput(response);
+
+                $('#fields_modal').modal('show');
             } catch (error) {
                 notifyOnError(error);
             }
+        }
+
+        function editInput(response) {
+            const data = response.data;
+            $.each(data, function(key, value) {
+                const $checkbox = $('input[type="checkbox"][name="' + key + '"]');
+                if ($checkbox.length) {
+                    $checkbox.prop('checked', value == 1 || value === true);
+                } else {
+                    $('[name="' + key + '"]').val(value);
+                }
+            });
         }
     </script>
 @endpush
