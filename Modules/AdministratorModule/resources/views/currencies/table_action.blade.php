0a07a82e7f748e8fc89f9220bcb976cb58f871ab
palindungan <rizkika.palindungan1@gmail.com>
2026-03-31 10:00:42 +0700

    Update table_action.blade.php

---
+++ Modules/AdministratorModule/resources/views/currencies/table_action.blade.php
@@ -0,0 +1,8 @@
+<div class="form-button-action">
+    <button type="button" class="btn btn-link btn-primary btn-lg">
+        <i class="fa fa-edit"></i>
+    </button>
+    <button type="button" class="btn btn-link btn-danger">
+        <i class="fa fa-times"></i>
+    </button>
+</div>
