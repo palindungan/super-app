f7dba2b70b60c5e8287b503dc688d49b5ae2611f
palindungan <rizkika.palindungan1@gmail.com>
2026-03-31 10:24:25 +0700

    Update table_action.blade.php

--- Modules/AdministratorModule/resources/views/currencies/table_action.blade.php
+++ Modules/AdministratorModule/resources/views/currencies/table_action.blade.php
@@ -1,7 +1,6 @@
 <div class="form-button-action">
     <button type="button" class="btn btn-link btn-primary btn-lg"
         onclick="editAction(
-        '{{ route('administrator-currencies.show', $row->id) }}',
         '{{ route('administrator-currencies.update', $row->id) }}'
     )">
         <i class="fa fa-edit"></i>
