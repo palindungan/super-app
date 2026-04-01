12ef3b423af34b1119e80c55d0e3958a61c77ad7
palindungan <rizkika.palindungan1@gmail.com>
2026-04-01 08:57:37 +0700

    company

--- Modules/AdministratorModule/resources/views/companies/fields.blade.php
+++ Modules/AdministratorModule/resources/views/companies/fields.blade.php
@@ -8,7 +8,7 @@
                     <div class="form-group {{ $errors->has('code') ? 'has-error has-feedback' : '' }}">
                         <label for="code">Kode</label>
                         <input type="text" class="form-control form-control" autocomplete="off" id="code"
-                            name="code" value="{{ old('code', $role->code ?? '') }}" />
+                            name="code" value="{{ old('code', $company->code ?? '') }}" />
                         @error('code')
                             <small class="form-text text-muted text-danger">
                                 {{ $message }}
@@ -20,7 +20,7 @@
                     <div class="form-group {{ $errors->has('name') ? 'has-error has-feedback' : '' }}">
                         <label for="name">Nama</label>
                         <input type="text" class="form-control form-control" autocomplete="off" id="name"
-                            name="name" value="{{ old('name', $role->name ?? '') }}" />
+                            name="name" value="{{ old('name', $company->name ?? '') }}" />
                         @error('name')
                             <small class="form-text text-muted text-danger">
                                 {{ $message }}
