2282bd2ee3cf75f5138efb2d21177061308fc348
palindungan <rizkika.palindungan1@gmail.com>
2026-04-01 12:18:43 +0700

    https://select2.org/getting-started/installation

--- resources/views/components/resources/assets/fields.blade.php
+++ resources/views/components/resources/assets/fields.blade.php
@@ -1,5 +1,11 @@
+@push('styles')
+    <link href="{{ asset('assets/css/plugin/select2/select2.min.css') }}" rel="stylesheet" />
+@endpush
+
 @push('scripts')
     <script src="{{ asset('components/layouts/master_1/assets/js/plugin/bootstrap-notify/bootstrap-notify.min.js') }}">
     </script>
     @include('components.notifications.bootstrap_notify')
+
+    <script src="{{ asset('assets/js/plugin/select2/select2.min.js') }}"></script>
 @endpush
