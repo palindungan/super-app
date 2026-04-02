94169a3e3d5a3f5c64d4c58c047f4917036c8d9c
palindungan <rizkika.palindungan1@gmail.com>
2026-04-02 12:15:29 +0700

    Update edit_tab.blade.php

--- Modules/AdministratorModule/resources/views/companies/edit_tab.blade.php
+++ Modules/AdministratorModule/resources/views/companies/edit_tab.blade.php
@@ -1,38 +1,35 @@
-<div class="card">
-    <div class="card-body" style="padding: 10px;">
-        <ul class="nav nav-pills nav-secondary  nav-pills-no-bd nav-pills-icons justify-content-center" id="nav-tab"
-            role="tablist">
-            <li class="nav-item" role="presentation">
-                <a data-bs-toggle="pill" role="tab" class="nav-link active" aria-selected="true" id="nav-index-0-tab"
-                    href="#nav-index-0" aria-controls="pills-index-0">
-                    Cabang
-                </a>
-            </li>
-            <li class="nav-item" role="presentation">
-                <a data-bs-toggle="pill" role="tab" class="nav-link " aria-selected="false" id="nav-index-1-tab"
-                    href="#nav-index-1" aria-controls="pills-index-1" tabindex="-1">
-                    Pengguna
-                </a>
-            </li>
-            <li class="nav-item" role="presentation">
-                <a data-bs-toggle="pill" role="tab" class="nav-link " aria-selected="false" id="nav-index-2-tab"
-                    href="#nav-index-2" aria-controls="pills-index-2" tabindex="-1">
-                    Saldo
-                </a>
-            </li>
-        </ul>
-        {{--
-        <div class="tab-content mt-3 mb-3" id="nav-tabContent">
-            <div role="tabpanel" class="tab-pane fade show active" id="nav-index-0" aria-labelledby="nav-index-0-tab">
-                Cabang
-            </div>
-            <div role="tabpanel" class="tab-pane fade " id="nav-index-1" aria-labelledby="nav-index-1-tab">
-                Pengguna
-            </div>
-            <div role="tabpanel" class="tab-pane fade " id="nav-index-2" aria-labelledby="nav-index-2-tab">
-                Saldo
-            </div>
+<div class="text-center">
+    <div class="card d-inline-block">
+        <div class="card-body">
+            <ul class="nav nav-pills nav-secondary nav-pills-no-bd nav-pills-icons">
+                <li class="nav-item">
+                    <a data-bs-toggle="pill" class="nav-link active" id="nav-index-0-tab" href="#nav-index-0">
+                        Cabang
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a data-bs-toggle="pill" class="nav-link" id="nav-index-1-tab" href="#nav-index-1">
+                        Pengguna
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a data-bs-toggle="pill" class="nav-link" id="nav-index-2-tab" href="#nav-index-2">
+                        Saldo
+                    </a>
+                </li>
+            </ul>
         </div>
-        --}}
+    </div>
+</div>
+
+<div class="tab-content mt-3 mb-3" id="nav-tabContent">
+    <div role="tabpanel" class="tab-pane fade show active" id="nav-index-0" aria-labelledby="nav-index-0-tab">
+        Cabang
+    </div>
+    <div role="tabpanel" class="tab-pane fade " id="nav-index-1" aria-labelledby="nav-index-1-tab">
+        Pengguna
+    </div>
+    <div role="tabpanel" class="tab-pane fade " id="nav-index-2" aria-labelledby="nav-index-2-tab">
+        Saldo
     </div>
 </div>
