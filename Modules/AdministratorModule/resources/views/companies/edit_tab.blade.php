38f1481283cd99fe442cca220d7c3ea82bc50a4c
palindungan <rizkika.palindungan1@gmail.com>
2026-04-02 12:16:37 +0700

    Update edit_tab.blade.php

--- Modules/AdministratorModule/resources/views/companies/edit_tab.blade.php
+++ Modules/AdministratorModule/resources/views/companies/edit_tab.blade.php
@@ -3,17 +3,17 @@
         <div class="card-body">
             <ul class="nav nav-pills nav-secondary nav-pills-no-bd nav-pills-icons">
                 <li class="nav-item">
-                    <a data-bs-toggle="pill" class="nav-link active" id="nav-index-0-tab" href="#nav-index-0">
+                    <a data-bs-toggle="pill" class="nav-link m-0 active" id="nav-index-0-tab" href="#nav-index-0">
                         Cabang
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a data-bs-toggle="pill" class="nav-link" id="nav-index-1-tab" href="#nav-index-1">
+                    <a data-bs-toggle="pill" class="nav-link m-0" id="nav-index-1-tab" href="#nav-index-1">
                         Pengguna
                     </a>
                 </li>
                 <li class="nav-item">
-                    <a data-bs-toggle="pill" class="nav-link" id="nav-index-2-tab" href="#nav-index-2">
+                    <a data-bs-toggle="pill" class="nav-link m-0" id="nav-index-2-tab" href="#nav-index-2">
                         Saldo
                     </a>
                 </li>
