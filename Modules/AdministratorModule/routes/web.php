18bb73135a636635e3dfcf028d41764773800d36
palindungan <rizkika.palindungan1@gmail.com>
2026-04-02 13:37:20 +0700

    Update web.php

--- Modules/AdministratorModule/routes/web.php
+++ Modules/AdministratorModule/routes/web.php
@@ -7,7 +7,9 @@ use Modules\AdministratorModule\Http\Controllers\RoleController;
 
 Route::prefix('administrator')->middleware(['auth'])->group(function () {
     Route::resource('companies', CompanyController::class)->names('administrator-companies');
+
     Route::resource('currencies', CurrencyController::class)->names('administrator-currencies');
     Route::get('currencies-select2', [CurrencyController::class, 'select2'])->name('administrator-currencies.select2');
+
     Route::resource('roles', RoleController::class)->names('administrator-roles');
 });
