9cfe62eb47d768a2eb482071a2e87efc05be1264
palindungan <rizkika.palindungan1@gmail.com>
2026-04-02 12:57:09 +0700

    Update edit_tab.blade.php

--- Modules/AdministratorModule/resources/views/companies/edit_tab.blade.php
+++ Modules/AdministratorModule/resources/views/companies/edit_tab.blade.php
@@ -36,17 +36,17 @@
             <div style="overflow-x: auto;" id="nav-scroll">
                 <ul class="nav nav-pills nav-secondary nav-pills-no-bd nav-pills-icons flex-nowrap">
                     <li class="nav-item">
-                        <a data-bs-toggle="pill" href="#nav-index-0" class="nav-link m-0 mb-3 active">
+                        <a class="nav-link m-0 mb-3" href="{{ url()->current() }}?tab=branches">
                             Cabang
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a data-bs-toggle="pill" href="#nav-index-1" class="nav-link m-0 mb-3">
+                        <a class="nav-link m-0 mb-3" href="{{ url()->current() }}?tab=users">
                             Pengguna
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a data-bs-toggle="pill" href="#nav-index-2" class="nav-link m-0 mb-3">
+                        <a class="nav-link m-0 mb-3" href="{{ url()->current() }}?tab=balances">
                             Saldo
                         </a>
                     </li>
@@ -56,6 +56,7 @@
         </div>
     </div>
 </div>
+
 {{-- <div class="tab-content mt-3 mb-3" id="nav-tabContent">
     <div role="tabpanel" id="nav-index-0" aria-labelledby="nav-index-0-tab" class="tab-pane fade show active">
         Cabang
