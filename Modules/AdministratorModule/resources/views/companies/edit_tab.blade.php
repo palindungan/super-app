91100705769d61ad04f085cb0fc494c320441493
palindungan <rizkika.palindungan1@gmail.com>
2026-04-02 10:59:22 +0700

    Update edit_tab.blade.php

--- Modules/AdministratorModule/resources/views/companies/edit_tab.blade.php
+++ Modules/AdministratorModule/resources/views/companies/edit_tab.blade.php
@@ -7,111 +7,24 @@
                     <li class="nav-item" role="presentation">
                         <a data-bs-toggle="pill" role="tab" class="nav-link active" aria-selected="true"
                             id="line-index-0-tab" href="#line-index-0" aria-controls="pills-index-0">
-                            Utama
-                            <span class="badge badge-secondary">1</span>
+                            Tab 1
                         </a>
                     </li>
                     <li class="nav-item" role="presentation">
                         <a data-bs-toggle="pill" role="tab" class="nav-link " aria-selected="false"
                             id="line-index-1-tab" href="#line-index-1" aria-controls="pills-index-1" tabindex="-1">
-                            Administrator
-                            <span class="badge badge-secondary">2</span>
+                            Tab 2
                         </a>
                     </li>
                 </ul>
                 <div class="tab-content mt-3 mb-3" id="line-tabContent">
                     <div role="tabpanel" class="tab-pane fade show active" id="line-index-0"
                         aria-labelledby="line-index-0-tab" style="padding-top: 15px;">
-                        <div class="accordion accordion-secondary">
-                            <div class="card">
-                                <div class="card-header" id="headingindex-0-0" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseindex-0-0" aria-expanded="true"
-                                    aria-controls="collapseindex-0-0">
-                                    <div class="span-title">
-                                        Beranda
-                                    </div>
-                                    <div class="span-mode"></div>
-                                </div>
-                                <div id="collapseindex-0-0" class="collapse show" aria-labelledby="headingindex-0-0"
-                                    data-parent="#accordion">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-md-3">
-                                                <div class="form-check">
-                                                    <input type="checkbox" class="form-check-input permissions"
-                                                        id="permissionsindex-0-0-0" name="permissions[]"
-                                                        value="home.index" checked="">
-                                                    <label class="form-check-label" for="permissionsindex-0-0-0">
-                                                        Lihat Apa Saja
-                                                    </label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        TAB 1
                     </div>
                     <div role="tabpanel" class="tab-pane fade " id="line-index-1" aria-labelledby="line-index-1-tab"
                         style="padding-top: 15px;">
-                        <div class="accordion accordion-secondary">
-                            <div class="card">
-                                <div class="card-header" id="headingindex-1-0" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseindex-1-0" aria-expanded="true"
-                                    aria-controls="collapseindex-1-0">
-                                    <div class="span-title">
-                                        Perusahaan
-                                    </div>
-                                    <div class="span-mode"></div>
-                                </div>
-                                <div id="collapseindex-1-0" class="collapse show" aria-labelledby="headingindex-1-0"
-                                    data-parent="#accordion">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-md-3">
-                                                <div class="form-check">
-                                                    <input type="checkbox" class="form-check-input permissions"
-                                                        id="permissionsindex-1-0-0" name="permissions[]"
-                                                        value="administrator-companies.index" checked="">
-                                                    <label class="form-check-label" for="permissionsindex-1-0-0">
-                                                        Lihat Apa Saja
-                                                    </label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="accordion accordion-secondary">
-                            <div class="card">
-                                <div class="card-header" id="headingindex-1-1" data-bs-toggle="collapse"
-                                    data-bs-target="#collapseindex-1-1" aria-expanded="true"
-                                    aria-controls="collapseindex-1-1">
-                                    <div class="span-title">
-                                        Mata Uang
-                                    </div>
-                                    <div class="span-mode"></div>
-                                </div>
-                                <div id="collapseindex-1-1" class="collapse show" aria-labelledby="headingindex-1-1"
-                                    data-parent="#accordion">
-                                    <div class="card-body">
-                                        <div class="row">
-                                            <div class="col-md-3">
-                                                <div class="form-check">
-                                                    <input type="checkbox" class="form-check-input permissions"
-                                                        id="permissionsindex-1-1-0" name="permissions[]"
-                                                        value="administrator-currencies.index" checked="">
-                                                    <label class="form-check-label" for="permissionsindex-1-1-0">
-                                                        Lihat Apa Saja
-                                                    </label>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        TAB 2
                     </div>
                 </div>
             </div>
