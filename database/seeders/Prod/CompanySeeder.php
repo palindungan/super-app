d1c3097fdb46ef11d631351506ffb6cb8e313e74
palindungan <rizkika.palindungan1@gmail.com>
2026-04-04 10:57:52 +0700

    Update CompanySeeder.php

--- database/seeders/Prod/CompanySeeder.php
+++ database/seeders/Prod/CompanySeeder.php
@@ -47,10 +47,27 @@ class CompanySeeder extends Seeder
         ];
 
         foreach ($data as $company_key => $company_value) {
+            // companies
             $company = Company::updateOrCreate(
-                ['code' => $company_value['code']],
-                $company_value
+                [
+                    'code' => $company_value['code'],
+                ],
+                [
+                    'default_currency_id' => $company_value['default_currency_id'],
+                    'code' => $company_value['code'],
+                    'name' => $company_value['name'],
+                ],
             );
+
+            // users
+            foreach ($company_value['users'] as $user_key => $user_value) {
+                $user = UserSeeder::updateOrCreate([
+                    'company_id' => $user_value['company_id'],
+                    'name' => $user_value['name'],
+                    'email' => $user_value['email'],
+                    'role_name' => $user_value['role_name'],
+                ]);
+            }
         }
     }
 }
