e72d7fe4589b9a66cbc44b1162e70bf69df2daeb
palindungan <rizkika.palindungan1@gmail.com>
2026-04-06 12:31:51 +0700

    Update web.php

--- Modules/AdministratorModule/routes/web.php
+++ Modules/AdministratorModule/routes/web.php
@@ -6,12 +6,13 @@ use Modules\AdministratorModule\Http\Controllers\CompanyController;
 use Modules\AdministratorModule\Http\Controllers\CurrencyController;
 use Modules\AdministratorModule\Http\Controllers\RoleController;
 
-Route::prefix('administrator')->middleware(['auth'])->group(function () {
-    Route::resource('companies', CompanyController::class)->names('administrator-companies');
-    Route::resource('companies.branches', BranchController::class)->names('administrator-companies-branches');
+Route::prefix('administrator')->middleware(['auth'])->name('administrator.')->group(function () {
+    Route::resource('companies', CompanyController::class);
 
-    Route::resource('currencies', CurrencyController::class)->names('administrator-currencies');
-    Route::get('currencies-select2', [CurrencyController::class, 'select2'])->name('administrator-currencies.select2');
+    Route::resource('companies.branches', BranchController::class);
 
-    Route::resource('roles', RoleController::class)->names('administrator-roles');
+    Route::resource('currencies', CurrencyController::class);
+    Route::get('currencies/select2', [CurrencyController::class, 'select2'])->name('currencies.select2');
+
+    Route::resource('roles', RoleController::class);
 });
