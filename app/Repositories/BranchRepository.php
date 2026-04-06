5220cc67caf227cabc024780da708e61dafe92bc
palindungan <rizkika.palindungan1@gmail.com>
2026-04-06 16:11:54 +0700

    Update BranchRepository.php

--- app/Repositories/BranchRepository.php
+++ app/Repositories/BranchRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Models\Branch;
+use Illuminate\Support\Facades\DB;
 
 class BranchRepository
 {
@@ -10,6 +11,17 @@ class BranchRepository
     {
         $query = Branch::query();
         $query->leftJoin('companies', 'companies.id', '=', 'branches.company_id');
+
+        $branch_user_counts = DB::table('branch_users')
+            ->select(
+                'branch_id',
+                DB::raw('COUNT(user_id) as count'),
+            )
+            ->groupBy('branch_id');
+        $query->leftJoinSub($branch_user_counts, 'branch_user_counts', function ($join) {
+            $join->on('branches.id', '=', 'branch_user_counts.branch_id');
+        });
+
         return $query;
     }
 }
