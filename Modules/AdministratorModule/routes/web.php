5d67dbcbb7522fd40ede65e63b2fa639b8f8ecf5
palindungan <rizkika.palindungan1@gmail.com>
2026-04-06 12:29:16 +0700

    Update web.php

--- Modules/AdministratorModule/routes/web.php
+++ Modules/AdministratorModule/routes/web.php
@@ -1,12 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\AdministratorModule\Http\Company\Controllers\BranchController;
 use Modules\AdministratorModule\Http\Controllers\CompanyController;
 use Modules\AdministratorModule\Http\Controllers\CurrencyController;
 use Modules\AdministratorModule\Http\Controllers\RoleController;
 
 Route::prefix('administrator')->middleware(['auth'])->group(function () {
     Route::resource('companies', CompanyController::class)->names('administrator-companies');
+    Route::resource('companies.branches', BranchController::class)->names('administrator-companies-branches');
 
     Route::resource('currencies', CurrencyController::class)->names('administrator-currencies');
     Route::get('currencies-select2', [CurrencyController::class, 'select2'])->name('administrator-currencies.select2');
