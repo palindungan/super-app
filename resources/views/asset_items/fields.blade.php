de8043ebe8f4e9c329f60b3141812132c20f410c
palindungan <rizkika.palindungan1@gmail.com>
2026-04-08 15:48:42 +0700

    Update fields.blade.php

--- resources/views/asset_items/fields.blade.php
+++ resources/views/asset_items/fields.blade.php
@@ -35,6 +35,57 @@
                                 <small class="form-text text-muted text-danger" id="name_error"></small>
                             </div>
                         </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group" id="asset_category_id_group">
+                                <label for="asset_category_id">Kategori</label>
+                                <input type="text" class="form-control form-control" autocomplete="off"
+                                    id="asset_category_id" name="asset_category_id" />
+                                <small class="form-text text-muted text-danger" id="asset_category_id_error"></small>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group" id="purchase_date_group">
+                                <label for="purchase_date">Tanggal Beli</label>
+                                <input type="text" class="form-control form-control" autocomplete="off"
+                                    id="purchase_date" name="purchase_date" />
+                                <small class="form-text text-muted text-danger" id="purchase_date_error"></small>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group" id="purchase_price_group">
+                                <label for="purchase_price">Harga Beli</label>
+                                <input type="text" class="form-control form-control" autocomplete="off"
+                                    id="purchase_price" name="purchase_price" />
+                                <small class="form-text text-muted text-danger" id="purchase_price_error"></small>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group" id="quantity_group">
+                                <label for="quantity">Jumlah</label>
+                                <input type="text" class="form-control form-control" autocomplete="off"
+                                    id="quantity" name="quantity" />
+                                <small class="form-text text-muted text-danger" id="quantity_error"></small>
+                            </div>
+                        </div>
+
+                        <div class="col-md-6">
+                            <div class="form-group" id="asset_status_id_group">
+                                <label for="asset_status_id">Status</label>
+                                <input type="text" class="form-control form-control" autocomplete="off"
+                                    id="asset_status_id" name="asset_status_id" />
+                                <small class="form-text text-muted text-danger" id="asset_status_id_error"></small>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group" id="photo_group">
+                                <label for="photo">Foto</label>
+                                <input type="text" class="form-control form-control" autocomplete="off"
+                                    id="photo" name="photo" />
+                                <small class="form-text text-muted text-danger" id="photo_error"></small>
+                            </div>
+                        </div>
                     </div>
                 </div>
                 <div class="modal-footer border-0">
