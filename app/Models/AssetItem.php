43274e5468938e8bc2c6784bac3a7aad4ac0a627
palindungan <rizkika.palindungan1@gmail.com>
2026-04-08 16:11:24 +0700

    Update AssetItem.php

--- app/Models/AssetItem.php
+++ app/Models/AssetItem.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class AssetItem extends Model
 {
-    //
+    protected $fillable = [
+        'asset_category_id',
+        'asset_status_id',
+        'code',
+        'name',
+        'purchase_date',
+        'purchase_price',
+        'quantity',
+        'photo',
+    ];
 }
