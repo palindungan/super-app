2fdbd290faf5d40419751fe633f74e923cde6226
palindungan <rizkika.palindungan1@gmail.com>
2026-04-10 22:18:11 +0700

    Update table_action.blade.php

---
+++ resources/views/asset_items/table_action.blade.php
@@ -0,0 +1,16 @@
+<div class="form-button-action">
+    <button type="button" class="btn btn-link btn-primary btn-lg"
+        onclick="editAction(
+            $('#fields_form'), $('#fields_modal'),
+            '{{ route('asset_items.update', $row->id) }}'
+        )">
+        <i class="fa fa-edit"></i>
+    </button>
+    <button type="button" class="btn btn-link btn-danger"
+        onclick="destroyAction(
+            '{{ route('asset_items.destroy', $row->id) }}', 
+            'Barang {{ $row->name }}'
+        )">
+        <i class="fa fa-times"></i>
+    </button>
+</div>
