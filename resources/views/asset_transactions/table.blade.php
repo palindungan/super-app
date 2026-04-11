b1596e383dbb5c392c04aba3f6a9d29fd5777a24
palindungan <rizkika.palindungan1@gmail.com>
2026-04-11 11:06:53 +0700

    erd3

---
+++ resources/views/asset_transactions/table.blade.php
@@ -0,0 +1,18 @@
+<div class="table-responsive">
+    <table id="datatable" class="display table table-striped table-hover">
+        <thead>
+            <tr>
+                <th>Kode</th>
+                <th>Tanggal</th>
+                <th>Keterangan</th>
+                <th>Lokasi Asal</th>
+                <th>Lokasi Tujuan</th>
+                <th>Tanggal Beli</th>
+                <th>Harga Beli</th>
+                <th>Foto</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody></tbody>
+    </table>
+</div>
