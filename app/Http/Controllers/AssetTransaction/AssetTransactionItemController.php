ef7ddf439b4cbf0de9083c35a815d279fc35b101
palindungan <rizkika.palindungan1@gmail.com>
2026-04-11 11:58:09 +0700

    AssetTransactionItemController

---
+++ app/Http/Controllers/AssetTransaction/AssetTransactionItemController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\AssetTransaction;
+
+use App\Http\Controllers\Controller;
+use App\Models\AssetTransactionItem;
+use App\Http\Requests\AssetTransaction\StoreAssetTransactionItemRequest;
+use App\Http\Requests\AssetTransaction\UpdateAssetTransactionItemRequest;
+
+class AssetTransactionItemController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAssetTransactionItemRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AssetTransactionItem $assetTransactionItem)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AssetTransactionItem $assetTransactionItem)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateAssetTransactionItemRequest $request, AssetTransactionItem $assetTransactionItem)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AssetTransactionItem $assetTransactionItem)
+    {
+        //
+    }
+}
