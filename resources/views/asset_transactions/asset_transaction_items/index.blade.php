c530969792b4e655b507cb217f1687e0057fa13e
palindungan <rizkika.palindungan1@gmail.com>
2026-04-11 12:14:28 +0700

    asset_transaction_items\index.blade.php

--- resources/views/asset_transactions/asset_transaction_items/index.blade.php
+++ resources/views/asset_transactions/asset_transaction_items/index.blade.php
@@ -1,13 +1,20 @@
+@include('components.resources.assets.v1.index')
+
+@push('scripts')
+    @include('components.validation.script')
+@endpush
+
 <div class="row">
     <div class="col-md-12">
         <div class="card">
             <div class="card-header">
                 <div class="d-flex align-items-center">
                     <h4 class="card-title">Detail</h4>
-                    <a class="btn btn-primary btn-round ms-auto" href="">
+                    <button class="btn btn-primary btn-round ms-auto"
+                        onclick="createAction($('#fields_form'), $('#fields_modal'))">
                         <i class="fa fa-plus"></i>
                         Buat Detail
-                    </a>
+                    </button>
                 </div>
             </div>
             <div class="card-body">
@@ -17,4 +24,39 @@
     </div>
 </div>
 
+@include('components.resources.assets.v1.simple_modal_create', [
+    'url' => route('asset_transactions.asset_transaction_items.store', $asset_transaction->id),
+])
 @include('components.resources.assets.v1.simple_modal_destroy')
+@include('components.resources.assets.v1.simple_modal_edit')
+@include('components.resources.assets.v1.simple_modal_show')
+@push('scripts')
+    <script>
+        function editInput($form, response) {
+            const data = response.data;
+            $.each(data, function(key, value) {
+                const $field = $form.find('[id="' + key + '"]');
+
+                if (!$field.length) return;
+
+                // jika input type file jangan diisi
+                if ($field.attr('type') === 'file') {
+                    return;
+                }
+
+                if ($field.attr('type') === 'checkbox') {
+                    $field.prop('checked', value == 1 || value === true);
+                } else {
+                    $field.val(value);
+                }
+            });
+
+            editTitleData(data);
+        }
+    </script>
+    <script>
+        function editTitleData(data) {
+            $('#edit_title_data').text(data.code);
+        }
+    </script>
+@endpush
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AssetItemController;
+use App\Http\Controllers\AssetTransaction\AssetTransactionItemController;
 use App\Http\Controllers\AssetTransactionController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
@@ -8,6 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth'])->group(function () {
     Route::get('', [HomeController::class, 'index'])->name('home');
 
+    Route::resource('asset_transactions.asset_transaction_items', AssetTransactionItemController::class);
     Route::resource('asset_transactions', AssetTransactionController::class);
 
     Route::get('asset_items/exportExcel', [AssetItemController::class, 'exportExcel'])->name('asset_items.exportExcel');
