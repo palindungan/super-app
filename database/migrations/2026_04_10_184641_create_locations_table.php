de223ab41ad1316bc9c32eb6f967ca7ea2c3d13e
palindungan <rizkika.palindungan1@gmail.com>
2026-04-11 11:03:04 +0700

    Update 2026_04_10_184641_create_locations_table.php

--- database/migrations/2026_04_10_184641_create_locations_table.php
+++ database/migrations/2026_04_10_184641_create_locations_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('locations', function (Blueprint $table) {
             $table->id();
 
+            $table->string('code')->nullable();
             $table->string('name')->nullable();
             $table->text('address')->nullable();
 
