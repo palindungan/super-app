0f82d198f67f64c431c4a470f8a50370282aa178
palindungan <rizkika.palindungan1@gmail.com>
2026-04-11 11:44:10 +0700

    Update table_action.blade.php

--- resources/views/asset_transactions/table_action.blade.php
+++ resources/views/asset_transactions/table_action.blade.php
@@ -5,7 +5,7 @@
     <button type="button" class="btn btn-link btn-danger"
         onclick="destroyAction(
             '{{ route('asset_transactions.destroy', $row->id) }}', 
-            'Peran {{ $row->name }}'
+            'Aset Transaksi {{ $row->name }}'
         )">
         <i class="fa fa-times"></i>
     </button>
